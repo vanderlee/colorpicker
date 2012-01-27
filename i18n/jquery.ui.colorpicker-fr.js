50dec88c441976321ebe0951671e4f00202b56d8
Antoine Proulx <proulx.antoine@gmail.com>
2012-01-27 16:56:14 -0500

    Add French language.

---
+++ i18n/jquery.ui.colorpicker-fr.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+	$.colorpicker.regional['fr'] = {
+		done:			       'Valider',
+		none:			       'Aucune couleur',
+		revert:			     'Couleur',
+		button:			     'Couleur',
+		title:			     'Choisir une couleur',
+		transparent:	   'Transparent',
+		hueShort:		     'T',
+		saturationShort: 'S',
+		valueShort:		   'V',
+		redShort:		     'R',
+		greenShort:		   'V',
+		blueShort:		   'B',
+		alphaShort:		   'A'
+	};
+});
