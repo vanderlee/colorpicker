735d2cdbaf39baccb9c4e80e0c0ebd41f7e5c181
Alexander Shepetko <a@shepetko.com>
2016-12-23 12:55:38 +0200

    Ukrainian localization

---
+++ i18n/jquery.ui.colorpicker-uk.js
@@ -0,0 +1,27 @@
+;jQuery(function ($) {
+    $.colorpicker.regional['uk'] = {
+        ok: 'ОК',
+        cancel: 'Скасувати',
+        none: 'Ніякий',
+        button: 'Колір',
+        title: 'Обрати колір',
+        transparent: 'Прозорий',
+        hsvH: 'H',
+        hsvS: 'S',
+        hsvV: 'V',
+        rgbR: 'R',
+        rgbG: 'G',
+        rgbB: 'B',
+        labL: 'L',
+        labA: 'a',
+        labB: 'b',
+        hslH: 'H',
+        hslS: 'S',
+        hslL: 'L',
+        cmykC: 'C',
+        cmykM: 'M',
+        cmykY: 'Y',
+        cmykK: 'K',
+        alphaA: 'A'
+    };
+});
