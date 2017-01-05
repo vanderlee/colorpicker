c5cebb8df827ee87d20cee48062875d6712be7b1
Krzysztof Niziol <krzysztof.niziol@meritoo.pl>
2017-01-05 17:19:50 +0100

    i18n - polish language

---
+++ i18n/jquery.ui.colorpicker-pl.js
@@ -0,0 +1,27 @@
+;jQuery(function($) {
+	$.colorpicker.regional['pl'] = {
+		ok:				'OK',
+		cancel:			'Anuluj',
+		none:			'Wyczyść',
+		button:			'Kolor',
+		title:			'Wskaż kolor',
+		transparent:	'Przezroczysty',
+		hsvH:			'H',
+		hsvS:			'S',
+		hsvV:			'V',
+		rgbR:			'R',
+		rgbG:			'G',
+		rgbB:			'B',
+		labL:			'L',
+		labA:			'a',
+		labB:			'b',
+		hslH:			'H',
+		hslS:			'S',
+		hslL:			'L',
+		cmykC:			'C',
+		cmykM:			'M',
+		cmykY:			'Y',
+		cmykK:			'K',
+		alphaA:			'A'
+	};
+});
